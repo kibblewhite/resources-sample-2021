6f7723e2df5aebf025d1cd3624827df6a9c39d73
@kibblewhite <kibblewhite@live.com>
2021-12-08 00:52:53 +0100

    Wanted to demonstrate that the resource strings can also be obtained by using property names by using a predefined variable

--- Program.cs
+++ Program.cs
@@ -4,9 +4,10 @@ Console.WriteLine($"CurrentThread.CurrentUICulture: {Thread.CurrentThread.Curren
 Console.WriteLine($"Direct Test Value for Culture: {Resource.Test}");
 Console.WriteLine(" -------------------- ");
 
+string name_of_property = "Test";
 Thread.CurrentThread.CurrentUICulture = CultureInfo.CreateSpecificCulture("es-ES");
 Console.WriteLine($"CurrentThread.CurrentUICulture: {Thread.CurrentThread.CurrentUICulture.Name}");
-Console.WriteLine($"Direct Test Value for Culture: {Resource.Test}");
+Console.WriteLine($"Direct Test Value for Culture: {Resource.ResourceManager.GetString(name_of_property)}");
 Console.WriteLine(" -------------------- ");
 
 Thread.CurrentThread.CurrentUICulture = CultureInfo.CreateSpecificCulture("en-US");
